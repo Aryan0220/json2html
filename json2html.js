f4c47cb5a87d11aa9cc882e7f6187d7f0775d2db
Aryan Bargal <114094596+Aryan0220@users.noreply.github.com>
2024-11-06 14:32:09 +0530

    Add files via upload

---
+++ json2html.js
@@ -0,0 +1,27 @@
+export default function json2html(data) {
+  const headers = Array.from(new Set(data.flatMap(Object.keys)));
+
+  let html = `<table data-user="aryan.gorde4@gmail.com">\n`;
+  html += "  <thead>\n";
+  html += "    <tr>";
+
+  headers.forEach((header) => {
+    html += `<th>${header}</th>`;
+  });
+  html += "</tr>\n";
+  html += "  </thead>\n";
+  html += "  <tbody>\n";
+
+  data.forEach((row) => {
+    html += "    <tr>";
+    headers.forEach((header) => {
+      html += `<td>${row[header] !== undefined ? row[header] : ""}</td>`;
+    });
+    html += "</tr>\n";
+  });
+
+  html += "  </tbody>\n";
+  html += "</table>";
+
+  return html;
+}
